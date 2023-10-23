4ccfd1d3bcc3b4674a5c60dd4a4ebca12ebf9a3a
moe beydoun <moebeydoun24@yahoo.com>
2023-10-22 20:29:04 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
-}
+    // Select the element you want to add an event listener to
+    const input = document.getElementById('button'); // Replace 'button' with the actual element's ID
+  
+    // Define the event listener function
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add the event listener for the 'click' event
+    input.addEventListener('click', clickAlert);
+  }
+  
+  addingEventListener();
